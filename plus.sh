7f0ef316826c8cd535d9c95a650b1433495cb60b
tjkim <xown3197@naver.com>
2022-11-08 21:06:08 +0900

    update

--- plus.sh
+++ plus.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+## VSCODE
+git clone https://github.com/JetsonHacksNano/installVSCode.git & cd installVSCode
+sh ./installVSCode.sh
+
+cd .. & rm -r installVSCode
+
 # [Jeton Nano] cv_bridge 에러 이슈
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1
 echo “export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1” >> ~/.bashrc
 source ~/.bashrc
 
