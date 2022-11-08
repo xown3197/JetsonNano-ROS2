e531a4e9689d75ceebc18c7510806689057e5032
tjkim <xown3197@naver.com>
2022-11-08 22:48:37 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # JetsonNano-ROS2
+
+### ROS2 설치
+```
+$ sh ./install_ros2_jetson_nano.sh
+$ sh ./plus.sh
+```
+
+### SD 카드 파티션 늘리는 방법
+
+[참고 사이트](https://omorobot.gitbook.io/manual/product/omo-r1mini/r1mini-pro/jetson-nano)
+```
+$ sudo apt update
+$ sudo apt install -y gparted
+$ sudo gparted
+```
+
+### test Real-time Detection 
+```
+$ source /opt/ros/foxy/setup.bash
+$ colcon build
+$ ./install/local_setup.bash
+```
+
+termainal 1
+```
+ros2 run image_tools cam2images
+```
+
+terminal 2
+```
+ros2 run live_detection live_detector
+```
--- plus.sh
+++ plus.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
 ## VSCODE
-git clone https://github.com/JetsonHacksNano/installVSCode.git & cd installVSCode
-sh ./installVSCode.sh
+git clone https://github.com/JetsonHacksNano/installVSCode.git & cd installVSCode & sh ./installVSCode.sh
+
 
 cd .. & rm -r installVSCode
 
 # [Jeton Nano] cv_bridge 에러 이슈
-export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1
-echo “export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1” >> ~/.bashrc
+echo "export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1" >> ~/.bashrc
 source ~/.bashrc
 
 # Vision task messages package 
 sudo apt install ros-foxy-vision-msgs
+
+## optional
+sudo apt install gedit
